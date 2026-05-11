2d80f23c2090087cfd537d6e0d2234022a11e392
Michael2024-coder <Agunbiademichael2017@gmail.com>
2026-05-12 01:24:40 +0200

    added withCredentials: true, to api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 });
 
 
@@ -17,4 +18,4 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
-// fix rename trigger
+

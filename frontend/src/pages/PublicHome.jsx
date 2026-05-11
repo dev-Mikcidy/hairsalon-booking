aa748c83d3e65af3a7105b6f9e6a8fa97adc9064
Michael2024-coder <Agunbiademichael2017@gmail.com>
2026-05-12 00:55:54 +0200

    added Public HomePage

--- frontend/src/pages/PublicHome.jsx
+++ frontend/src/pages/PublicHome.jsx
@@ -1,7 +1,7 @@
 export default function PublicHome() {
   return (
     <div className="container py-5 text-center">
-      <h1>Welcome to Our Salon</h1>
+      <h1>Welcome to Mike's Salon</h1>
       <p>Book your appointment online quickly and easily.</p>
 
       <a href="/book" className="btn btn-primary mt-3">
